27627ee79b45657e79d08d0a258b27d732503cff
IvanoWoii <rezaivano123@gmail.com>
2023-06-05 21:20:05 +0700

    minggu4

---
+++ minggu4/acara_15/app/src/main/java/com/example/acara_15/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.acara_15;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private List<mahasiswa> mahasiswaList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mahasiswaList = addData();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        com.example.acara_15.adapter adapter = new adapter((ArrayList<mahasiswa>) mahasiswaList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+
+
+    private List<mahasiswa> addData() {
+        List<mahasiswa> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+        mahasiswaList.add(new mahasiswa("Muhammad Reza Ivano Pahlevi", "E41211245", "08121651223"));
+
+
+
+
+
+        return mahasiswaList;
+    }
+}
